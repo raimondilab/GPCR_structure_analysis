9d140056331d7f3c6cab981b2c3362ea7aba0c0d
maticmarin <marin.matic@sns.it>
2022-09-09 12:09:45 -0600

    Add files via upload

---
+++ contacts/rmsd_script/rmsf.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+./extract_rmsf.py bioalign_gprot_all_gpcr_all
+./extract_rmsf.py bioalign_gprot_cons_gpcr_all 
+./extract_rmsf.py bioalign_gprot_cons_gpcr_all12 
+./extract_rmsf.py bioalign_gprot_all_gpcr_all12
