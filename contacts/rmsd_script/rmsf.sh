b088cb914d64a4ba60c82d75f7f5cb97f972643e
maticmarin <marin.matic@sns.it>
2022-09-14 05:01:55 -0600

    Delete rmsf.sh

--- contacts/rmsd_script/rmsf.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-./extract_rmsf.py bioalign_gprot_all_gpcr_all
-./extract_rmsf.py bioalign_gprot_cons_gpcr_all 
-./extract_rmsf.py bioalign_gprot_cons_gpcr_all12 
-./extract_rmsf.py bioalign_gprot_all_gpcr_all12
