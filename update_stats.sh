07defaefd88f79c4d2e0241ed7942704eaed408f
PasqM <pasqmigl97@gmail.com>
2023-04-17 12:31:54 +0200

    Add interface energy information

--- update_stats.sh
+++ update_stats.sh
@@ -2,6 +2,7 @@
 
 source activate bioinfo
 
+# Remove the antibody chain from the structures
 for pdb in `ls -1t ../GPCR_experimental_structures/structures/*cif`
 do
     pdbout=`echo $pdb | cut -f 4 -d "/"`
@@ -12,13 +13,18 @@ do
 	python3 interface_energy/remove_ab.py $pdb ../GPCR_experimental_structures/structures_no_Ab/${pdbout%%.cif}.pdb
 done
 
-# Compute relaxed structures and contacts (if not already available)
 echo "Relaxing structures and computing interface energies..."
-qsub -Wblock=true interface_energy/interface_energy.sh
-python3 interface_energy/find_interface_energy.py GPCR_structs_clean.tsv ../GPCR_experimental_structures/binding_energy.tsv
+#qsub -Wblock=true interface_energy/interface_energy.sh
+
+# Add the interface energies to the table
+python3 interface_energy/find_interface_energy.py GPCR_structs_clean.tsv binding_energy.tsv
+mv binding_energy.tsv GPCR_structs_clean.tsv
+
+# Find the best structure for each GPCR-Gprotein complex
+python3 complex_selection/find_consensus.py
 
 # Draw figures
 echo "Drawing figures..."
 mkdir ../GPCR_experimental_structures/figures
-python3 interface_energy/interface_energy_plot.py > ../GPCR_experimental_structures/stats.txt
-python3 interface_energy/interface_energy_plot_classA.py > ../GPCR_experimental_structures/stats_classA.txt
+python3 interface_energy/interface_energy_plot.py
+python3 interface_energy/interface_energy_plot_classA.py
